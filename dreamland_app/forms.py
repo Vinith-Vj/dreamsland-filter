e2870f8429439869603ceb741d1650500679fcc6
Vinith-Vj <vinithvj555@gmail.com>
2025-03-05 16:03:13 +0530

    all of the styles added in forms got removed

--- dreamland_app/forms.py
+++ dreamland_app/forms.py
@@ -7,33 +7,33 @@
 #     address = forms.CharField(label='Comments', widget=forms.Textarea, max_length=500)
 
 
-from django import forms
-from .models import Property, Location
+# from django import forms
+# from .models import Property, Location
 
-class PropertyForm(forms.ModelForm):
-    class Meta:
-        model = Property
-        fields = '__all__'
+# class PropertyForm(forms.ModelForm):
+#     class Meta:
+#         model = Property
+#         fields = '__all__'
+#         widgets = {
+#             'property_name': forms.TextInput(attrs={'class': 'form-control'}),
+#             'property_price': forms.NumberInput(attrs={'class': 'form-control'}),
+#             'property_description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+#             'property_location': forms.Select(attrs={'class': 'form-select'}),  # Bootstrap 5 uses form-select
+#             'property_image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+#         }
 
-        widgets = {
-            'property_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'property_price': forms.NumberInput(attrs={'class': 'form-control'}),
-            'property_description': forms.Textarea(attrs={'class': 'form-control'}),
-            'property_location': forms.Select(attrs={'class': 'form-control'}),
-            'property_image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
-        }
+#     def __init__(self, *args, **kwargs):
+#         locations_queryset = kwargs.pop('locations_queryset', Location.objects.none())
+#         super(PropertyForm, self).__init__(*args, **kwargs)
 
-    def __init__(self, *args, **kwargs):
-        locations_queryset = kwargs.pop('locations_queryset', Location.objects.none())
-        super(PropertyForm, self).__init__(*args, **kwargs)
+#         self.fields['property_location'] = forms.ModelChoiceField(
+#             queryset=locations_queryset,
+#             required=True,
+#             label="Property Location",
+#             widget=forms.Select(attrs={'class': 'form-select'})  # For dropdowns
+#         )
 
-        # Filter property_location field
-        self.fields['property_location'] = forms.ModelChoiceField(
-            queryset=locations_queryset,
-            required=True,
-            label="Property Location",
-             widget=forms.Select(attrs={'class': 'form-control'})  # Add class here too
-        )
+
 
 # from django import forms
 # from .models import Property, Location
@@ -43,6 +43,14 @@ class PropertyForm(forms.ModelForm):
 #         model = Property
 #         fields = '__all__'
 
+#         widgets = {
+#             'property_name': forms.TextInput(attrs={'class': 'form-control'}),
+#             'property_price': forms.NumberInput(attrs={'class': 'form-control'}),
+#             'property_description': forms.Textarea(attrs={'class': 'form-control'}),
+#             'property_location': forms.Select(attrs={'class': 'form-control'}),
+#             'property_image': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+#         }
+
 #     def __init__(self, *args, **kwargs):
 #         locations_queryset = kwargs.pop('locations_queryset', Location.objects.none())
 #         super(PropertyForm, self).__init__(*args, **kwargs)
@@ -51,5 +59,25 @@ class PropertyForm(forms.ModelForm):
 #         self.fields['property_location'] = forms.ModelChoiceField(
 #             queryset=locations_queryset,
 #             required=True,
-#             label="Property Location"
+#             label="Property Location",
+#              widget=forms.Select(attrs={'class': 'form-control'})  # Add class here too
 #         )
+
+from django import forms
+from .models import Property, Location
+
+class PropertyForm(forms.ModelForm):
+    class Meta:
+        model = Property
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        locations_queryset = kwargs.pop('locations_queryset', Location.objects.none())
+        super(PropertyForm, self).__init__(*args, **kwargs)
+
+        # Filter property_location field
+        self.fields['property_location'] = forms.ModelChoiceField(
+            queryset=locations_queryset,
+            required=True,
+            label="Property Location"
+        )
